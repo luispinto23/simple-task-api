86c8f2af74f63769090e663f7de38fa862400eb9
Luís Pinto <luispinto23@gmail.com>
2021-11-09 22:17:02 +0000

    refactor: refactor tests

--- app/tests/app.test.js
+++ app/tests/app.test.js
@@ -1,12 +1,34 @@
 const request = require('supertest');
 const app = require('../app');
+const { encryptMessage, decryptMessage } = require('../crypto');
 
 let managerAuthToken;
 let techAuthToken;
 
+let managerTaskId;
+let techTaskId;
+
 describe('Sword task api', () => {
-  it.only('POST /auth/login as a manager', async () => {
-    const response = await request(app)
+  it('POST /auth/login as a manager', async () => {
+    const { body } = await request(app)
+      .post('/auth/login')
+      .send({
+        email: 'manager@sword.com',
+        password: process.env.DEFAULT_PASSWORD,
+      })
+      .expect(200);
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        token: expect.any(String),
+      })
+    );
+
+    managerAuthToken = body.token;
+  });
+
+  it('POST /auth/login as a tech', async () => {
+    const { body } = await request(app)
       .post('/auth/login')
       .send({
         email: 'tech@sword.com',
@@ -14,76 +36,72 @@ describe('Sword task api', () => {
       })
       .expect(200);
 
-    expect(response.body).toEqual(
+    expect(body).toEqual(
       expect.objectContaining({
         token: expect.any(String),
       })
     );
+
+    techAuthToken = body.token;
   });
 
-  it.todo('POST /login as a tech');
-
-  it.todo('POST /task as a manager');
-  it.todo('POST /task as a tech');
-
-  it.todo('PUT /task as a manager');
-  it.todo('PUT /task as a tech');
-  it.todo('PUT /task from tech as a manager');
-  it.todo('PUT /task from manager as a tech');
-  it.todo('PUT /task from other user');
-
-  it.todo('GET /task all tasks as a manager');
-  it.todo('GET /task all tasks as a tech');
-
-  it.todo('GET /task by id as a manager');
-  it.todo('GET /task by id as a tech');
-  it.todo('GET /task from tech by id as a manager');
-  it.todo('GET /task from other user as tech');
-
-  it('GET /tasks => Return a list of tasks', async () => {
-    const response = await request(app).get('/tasks').expect(200);
-
-    if (response.body.length) {
-      expect(response.body).toEqual(
-        expect.arrayContaining([
-          expect.objectContaining({
-            id: expect.any(Number),
-            description: expect.any(String),
-            createdAt: expect.any(String),
-            updatedAt: expect.any(String),
-            authorId: expect.any(Number),
-          }),
-        ])
-      );
-    } else {
-      expect(response.body).toEqual([]);
-    }
-  });
-  it('GET /tasks/:id => Return a specific task', async () => {
-    const response = await request(app).get('/tasks/1').expect(200);
-
-    expect(response.body).toEqual(
-      expect.objectContaining({
-        id: expect.any(Number),
-        description: expect.any(String),
-        createdAt: expect.any(String),
-        updatedAt: expect.any(String),
-        authorId: expect.any(Number),
-      })
-    );
-  });
-
-  it('POST /tasks => Create a new task', async () => {
-    const taskDate = new Date().getTime();
-    const response = await request(app)
+  it('POST /tasks as a manager', async () => {
+    const { body } = await request(app)
       .post('/tasks')
-      .send({ description: 'another task', authorId: 1 })
+      .send({
+        summary: 'Testing POST task as a manager',
+      })
+      .set('Authorization', managerAuthToken)
       .expect(201);
 
-    expect(response.body).toEqual(
+    expect(body).toEqual(
       expect.objectContaining({
         id: expect.any(Number),
-        description: 'another task',
+        summary: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        authorId: 1,
+      })
+    );
+
+    managerTaskId = body.id;
+  });
+
+  it('POST /tasks as a tech', async () => {
+    const { body } = await request(app)
+      .post('/tasks')
+      .send({
+        summary: 'Testing POST task as a technician',
+      })
+      .set('Authorization', techAuthToken)
+      .expect(201);
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        summary: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        authorId: 3,
+      })
+    );
+
+    techTaskId = body.id;
+  });
+
+  it('PUT /tasks as a manager', async () => {
+    const { body } = await request(app)
+      .put(`/tasks/${managerTaskId}`)
+      .send({
+        summary: 'Test PUT task as a manager',
+      })
+      .set('Authorization', managerAuthToken)
+      .expect(200);
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: managerTaskId,
+        summary: expect.any(String),
         createdAt: expect.any(String),
         updatedAt: expect.any(String),
         authorId: 1,
@@ -91,26 +109,132 @@ describe('Sword task api', () => {
     );
   });
 
-  it('PUT /tasks/:id => Update a specific task', async () => {
-    const newDescription = 'new description';
-
-    const response = await request(app)
-      .put('/tasks/1')
-      .send({ description: newDescription })
+  it('PUT /tasks as a tech', async () => {
+    const { body } = await request(app)
+      .put(`/tasks/${techTaskId}`)
+      .send({
+        summary: 'Test PUT task as a tech',
+      })
+      .set('Authorization', techAuthToken)
       .expect(200);
 
-    expect(response.body).toEqual(
+    expect(body).toEqual(
       expect.objectContaining({
-        id: expect.any(Number),
-        description: newDescription,
-        date: expect.any(Number),
-        userId: expect.any(Number),
+        id: techTaskId,
+        summary: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        authorId: 3,
       })
     );
   });
 
-  it('DELETE /tasks/:id => Delete a specific task', async () => {
-    await request(app).delete('/tasks/1').expect(200);
+  it('PUT /tasks from tech as a manager should fail', async () => {
+    return request(app)
+      .put(`/tasks/${techTaskId}`)
+      .send({
+        summary: 'Test PUT to a technician task as a manager',
+      })
+      .set('Authorization', managerAuthToken)
+      .expect(401);
+  });
+
+  it('PUT /tasks from manager as a tech should fail', async () => {
+    return request(app)
+      .put(`/tasks/${managerTaskId}`)
+      .send({
+        summary: 'Test PUT to a manager task as a technician',
+      })
+      .set('Authorization', techAuthToken)
+      .expect(401);
+  });
+
+  it('GET /tasks all tasks as a manager', async () => {
+    const { body } = await request(app)
+      .get(`/tasks`)
+      .set('Authorization', managerAuthToken)
+      .expect(200);
+
+    if (body.length) {
+      expect(body).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(Number),
+            summary: expect.any(String),
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+            authorId: expect.any(Number),
+          }),
+        ])
+      );
+    } else {
+      expect(body).toEqual([]);
+    }
+  });
+
+  it('GET /tasks all tasks as a tech', async () => {
+    return request(app)
+      .get(`/tasks`)
+      .set('Authorization', techAuthToken)
+      .expect(401);
+  });
+
+  it('GET /tasks by id as a manager', async () => {
+    const { body } = await request(app)
+      .get(`/tasks/${managerTaskId}`)
+      .set('Authorization', managerAuthToken)
+      .expect(200);
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: managerTaskId,
+        summary: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        authorId: 1,
+      })
+    );
+  });
+
+  it('GET /tasks by id as a tech', async () => {
+    const { body } = await request(app)
+      .get(`/tasks/${techTaskId}`)
+      .set('Authorization', techAuthToken)
+      .expect(200);
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: techTaskId,
+        summary: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        authorId: 3,
+      })
+    );
+  });
+
+  it('GET /tasks from a technician by id as a manager', async () => {
+    const { body } = await request(app)
+      .get(`/tasks/${techTaskId}`)
+      .set('Authorization', managerAuthToken)
+      .expect(200);
+
+    expect(body).toEqual(
+      expect.objectContaining({
+        id: techTaskId,
+        summary: expect.any(String),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String),
+        authorId: 3,
+      })
+    );
+  });
+
+  it('GET /tasks from other user as tech should fail', async () => {
+    return request(app)
+      .get(`/tasks/${managerTaskId}`)
+      .set('Authorization', techAuthToken)
+      .expect(401);
   });
 });
 
